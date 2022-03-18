0881299329e13f11e77afc210b0ad4271b0a277c
EbubekirAgacdan <94063552+EbubekirAgacdan@users.noreply.github.com>
2022-03-18 18:29:13 +0300

    Add files via upload

---
+++ SQL-Assignment-4.sql
@@ -0,0 +1,38 @@
+-----
+--Discount Effects
+
+--Generate a report including product IDs and discount effects on whether the increase in the discount rate positively impacts the number of orders for the products.
+
+--In this assignment, you are expected to generate a solution using SQL with a logical approach. 
+
+
+-----
+
+CREATE VIEW TBL
+AS      (
+
+		select distinct product_id,discount,
+				lead(discount) over (partition by product_id order by discount) next_discount,
+				sum(quantity) total_quantity,
+				lead(sum(quantity)) over (partition by product_id order by discount) next_quantity
+		from sale.order_item
+		group by product_id,discount
+		)
+
+
+
+
+
+with TBL_1 as (
+				select product_id,	
+						sum((next_quantity - total_quantity) / (next_discount - discount)) eðim
+				from TBL
+				group by product_id
+			  )
+SELECT product_id,
+	    CASE  
+		     WHEN eðim > 0 then 'Positive'
+			 WHEN eðim = 0 then 'Neutral'
+		     WHEN eðim < 0 then 'Negative'
+		END AS discount_effect
+FROM TBL_1
